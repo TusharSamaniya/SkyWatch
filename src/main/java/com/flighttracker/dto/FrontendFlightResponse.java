d485fbe3863a95a5251973abd425c29552e96555
TusharSamaniya <tushasamaniya4956@gmail.com>
2026-04-05 14:59:01 +0530

    The clean, combined object sent to React

---
+++ src/main/java/com/flighttracker/dto/FrontendFlightResponse.java
@@ -0,0 +1,24 @@
+package com.flighttracker.dto;
+
+public class FrontendFlightResponse {
+	
+	//live data from Opensky
+	private String icao24;
+	private String callsign;
+	private double latitude;
+	private double longitude;
+	private double altitude;
+	private double velocity;
+	private double trueTrack;
+	
+	//route data from Aviationstack
+	private String airlineName;
+	private String airineName;
+	private String flightNumber;
+	private String originAirport;
+	private String originIata;
+	private String destinationAirport;
+	private String destinationIata;
+	private String status;
+
+}
